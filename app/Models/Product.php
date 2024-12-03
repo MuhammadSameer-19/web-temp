cb1eece3af0b76d6d309c4ece7c239c1be67b508
MuhammadSameer-19 <mohdsameersomji@gmail.com>
2024-12-03 14:58:47 +0500

    Update Product.php
    Change relation between models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,14 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
     protected $table = 'Products';
     protected $fillable=['Pro_Name','Pro_Detail','Pro_Category','Pro_Price',	'Pro_Img'];
 
-    public function order():HasOne{
-        return $this->hasOne(Order::class);
+    public function order():HasMany{
+        return $this->hasMany(Order::class);
     }
 }
